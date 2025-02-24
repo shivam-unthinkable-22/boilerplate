8fe70aef1f7324bc664395139bfca0889d54a22c
Shivam <92pmv1uotsyy3q8zhr7ykhq195y6it@bots.bitbucket.org>
2025-02-24 10:43:46 +0530

    update: boilerplate

--- src/ui/Login/index.tsx
+++ src/ui/Login/index.tsx
@@ -1,28 +1,14 @@
 "use client";
 
-import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [error] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    const res = await signIn("credentials", {
-      redirect: false,
-      username: email,
-      password,
-    });
-
-    if (res?.error) {
-      setError("Invalid username or password");
-    } else {
-      // Redirect to home or another page on successful login
-      // window.location.href = "/";
-    }
   };
 
   return (
